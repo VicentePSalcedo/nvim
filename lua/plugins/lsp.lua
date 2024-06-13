02115bb583319a5b29a7a306d02c846dcf07f58f
Vicente <vicentepsalcedo@gmail.com>
2024-06-13 15:12:46 -0400

    Updated: 2024-06-13 15:12:46

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,32 @@
 return {
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
-    {'mason.nvim', enabled = false},
-    {'williamboman/mason-lspconfig.nvim', enabled = false},
+    { 'VonHeikemen/lsp-zero.nvim',         branch = 'v3.x' },
+    {
+        'neovim/nvim-lspconfig',
+        -- IMPORTANT: someone smarter than me got this workign so dont touch
+        -- this is used to enable typsciprt auto completes and highlights
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- Setup language servers.
+            local lspconfig = require("lspconfig")
+            lspconfig.tsserver.setup({ capabilities = capabilities })
+            lspconfig.eslint.setup({
+                on_attach = function(client, bufnr)
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        command = "EslintFixAll",
+                    })
+                end,
+                capabilities = capabilities
+            })
+            require("lspconfig").eslint.setup({})
+        end,
+        -- IMPORTANT --
+    },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'mason.nvim',                        enabled = false },
+    { 'williamboman/mason-lspconfig.nvim', enabled = false },
 }
